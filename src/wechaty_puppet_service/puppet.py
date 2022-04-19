c3c4299e9be68a9c80b02340f16d3b2beee231a1
骑马小猫 <1435130236@qq.com>
2022-04-19 18:19:49 +0800

    fix urllink messsage forward

--- src/wechaty_puppet_service/puppet.py
+++ src/wechaty_puppet_service/puppet.py
@@ -450,7 +450,10 @@ class PuppetService(Puppet):
             await self.message_send_text(conversation_id=to_id, message=payload.text)
         elif payload.type == MessageType.MESSAGE_TYPE_URL:
             url_payload = await self.message_url(message_id=message_id)
-            await self.message_send_url(conversation_id=to_id, url=url_payload.url)
+            await self.message_send_url(
+                conversation_id=to_id,
+                url=json.dumps(asdict(url_payload))
+            )
         elif payload.type == MessageType.MESSAGE_TYPE_MINI_PROGRAM:
             mini_program = await self.message_mini_program(message_id=message_id)
             await self.message_send_mini_program(conversation_id=to_id, mini_program=mini_program)
